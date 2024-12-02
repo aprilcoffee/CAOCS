ec993bd8ed26178f2a59ba7b20858663eb791350
Ting Chun Liu <8765498@gmail.com>
2024-12-02 20:41:52 +0100

    add port 4001

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const Datastore = require('nedb');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 4001;
 
 // Initialize the database
 const db = new Datastore({ 
